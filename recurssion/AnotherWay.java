429168f6b4f3c34af30f1208d61dada583a42ebe
kajalkumawatregexsoftware <kajalkumawat0404@gmail.com>
2024-12-03 12:59:21 +0530

    another way to print

---
+++ recurssion/AnotherWay.java
@@ -0,0 +1,13 @@
+public class AnotherWay {
+    public static void printNum(int n) {
+        if (n == 6) {
+            return;
+        }
+        System.out.println(n);
+        printNum(n + 1); 
+    }
+
+    public static void main(String[] args) {
+        printNum(1);
+    }
+}
