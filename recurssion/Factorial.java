8469958f70972d3dca6ea28861c838a1a3f95491
kajalkumawatregexsoftware <kajalkumawat0404@gmail.com>
2024-12-03 09:58:55 +0530

    factorial through recurssion

---
+++ recurssion/Factorial.java
@@ -0,0 +1,15 @@
+public class Factorial {
+    public static int facto(int n) {
+        if (n == 0) {
+            return 1;
+        }
+        int ans = facto(n - 1);
+        int f = ans * n;
+        return f;
+    }
+
+    public static void main(String[] args) {
+        int n = 5;
+        System.out.println(facto(n));
+    }
+}
